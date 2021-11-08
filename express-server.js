d935922dcd5ef2d34eb3b4baec0098c56457a3d6
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-11-08 23:07:17 +0000

    test returning JSON string of urlDatabase object

--- express-server.js
+++ express-server.js
@@ -11,6 +11,10 @@ app.get('/', (req, res) => {
   res.send('Hello there!');
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.listen(PORT, () => {
   console.log(`example app listening on port ${PORT}`);
 });
