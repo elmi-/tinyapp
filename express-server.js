19c321d6f2e52b460ffbb9bbc452ac41aed1f1a0
Abdi Elmi <abdi.elmi@rocketmail.com>
2021-11-09 16:45:56 +0000

    added form for new URLs and POST route to handle form submission; also implemented generateRandomString function

--- express-server.js
+++ express-server.js
@@ -1,16 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   'b2xVn2': 'http://www.lighthouselabs.ca',
   '9sm5xK': 'http://www.google.com'
 };
 
+function generateRandomString() {
+  const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let randomStr = '';
+  for(let i = 0; i < 6; i++) {
+    randomStr += chars.charAt(Math.random() * 
+ chars.length);
+  }
+  return randomStr;
+}
+
 app.get('/', (req, res) => {
   res.send('Hello there!');
+  console.log(generateRandomString());
 });
 
 app.get('/urls.json', (req, res) => {
@@ -22,10 +36,20 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
-  res.render("urls_show", templateVars);
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
 });
+
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+app.get('/urls/:shortURL', (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render('urls_show', templateVars);
+});
+
 
 app.listen(PORT, () => {
   console.log(`example app listening on port ${PORT}`);
